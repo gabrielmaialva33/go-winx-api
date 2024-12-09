53dc041dd6e4f6f876025c46daaa8d848826f3bd
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-12-09 01:41:28 -0300

    feat: add terminal package for password prompt functionality
    Introduced a new internal package `terminal` providing functions for securely prompting user input, specifically passwords. Updated `go.mod` and `go.sum` to include dependencies `golang.org/x/term` and `golang.org/x/sys` for terminal handling capabilities.

---
+++ internal/terminal/terminal.go
@@ -0,0 +1,52 @@
+package terminal
+
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/term"
+	"os"
+	"strings"
+	"syscall"
+)
+
+type Term struct{}
+
+type Terminal interface {
+	PromptPassword(promptMsg string) (string, error)
+}
+
+// PromptPassword prompts user to enter password and then returns it
+func (c *Term) PromptPassword(promptMsg string) (string, error) {
+	fmt.Print(promptMsg)
+	bytePassword, err := term.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		return "", err
+	}
+	fmt.Println()
+	return strings.TrimSpace(string(bytePassword)), nil
+}
+
+// PromptPassword prompts user to enter password and then returns it
+func PromptPassword(promptMsg string) (string, error) {
+	fmt.Print(promptMsg)
+	bytePassword, err := term.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		return "", err
+	}
+	fmt.Println()
+	return strings.TrimSpace(string(bytePassword)), nil
+}
+
+// InputPrompt receives a string value using the label
+func InputPrompt(label string) string {
+	var s string
+	r := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Fprint(os.Stderr, label+" ")
+		s, _ = r.ReadString('\n')
+		if s != "" {
+			break
+		}
+	}
+	return strings.TrimSpace(s)
+}
