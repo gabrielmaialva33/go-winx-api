a692ccd59bef5e868c2607b5b1a703cd702c841c
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-12-09 01:19:13 -0300

    chore: add ChannelID field to config struct
    Introduce a new ChannelID field in the config struct to handle channel-specific configurations. This field is marked as required, ensuring that configurations will not miss this crucial data.

--- config/config.go
+++ config/config.go
@@ -4,5 +4,6 @@ type Config struct {
 	ApiID          int32    `envconfig:"API_ID" required:"true"`
 	ApiHash        string   `envconfig:"API_HASH" required:"true"`
 	BotToken       string   `envconfig:"BOT_TOKEN" required:"true"`
+	ChannelID      int64    `envconfig:"CHANNEL_ID" required:"true"`
 	StringSessions []string `envconfig:"STRING_SESSIONS" required:"true"`
 }
