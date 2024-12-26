6cb70e08b4538e385950e40b28fc11375417e0fb
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-12-25 22:04:18 -0300

    refactor: GetAllPosts handler to include repository context
    Added a background context to the repository's GetHistory call within the GetAllPosts handler. Updated the response format to return a JSON object instead of a plain string. These changes improve functionality and response consistency.

--- internal/server/handlers/posts.go
+++ internal/server/handlers/posts.go
@@ -1,14 +1,19 @@
 package handlers
 
 import (
-	"github.com/gofiber/fiber/v2"
+	"context"
 	"go-winx-api/internal/telegram"
+
+	"github.com/gofiber/fiber/v2"
 	"go.uber.org/zap"
 )
 
 func GetAllPosts(log *zap.Logger, repository *telegram.Repository) fiber.Handler {
+	ctx := context.Background()
+	repository.GetHistory(ctx)
+
 	return func(c *fiber.Ctx) error {
 		log.Info("Get all posts")
-		return c.SendString("Get all posts")
+		return c.JSON(fiber.Map{"message": "Get all posts"})
 	}
 }
